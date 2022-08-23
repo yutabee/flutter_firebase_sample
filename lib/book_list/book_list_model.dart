4ac18ab6d380e0a5ac489a313b37b93c96266e03
yutabee <bsk87m@gmail.com>
2022-08-23 20:35:37 +0900

    firebase create

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -3,21 +3,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class BookListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _booksStream =
-      FirebaseFirestore.instance.collection('books').snapshots();
-
   List<Book>? books;
 
-  void fetchBookList() {
-    _booksStream.listen((QuerySnapshot snapshot) {
-      final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
-        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-        final String title = data['title'];
-        final String author = data['author'];
-        return Book(title, author);
-      }).toList();
-      this.books = books;
-      notifyListeners();
-    });
+  void fetchBookList() async {
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('books').get();
+
+    final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String title = data['title'];
+      final String author = data['author'];
+      return Book(title, author);
+    }).toList();
+
+    this.books = books;
+    notifyListeners();
   }
 }
